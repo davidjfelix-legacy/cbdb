6846b9e3053f97d781666f65f02f4705c5e67fdf
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-19 21:21:35 -0500

    Update hystrix.go

--- aggregator/hystrix.go
+++ aggregator/hystrix.go
@@ -67,28 +67,6 @@ type HystrixHistogram struct {
 }
 
 
-func (s SSEString) ParseHystrixStream() (HystrixStream, error) {
-	// The eventsource string isn't big short circuit
-	if len(s) < 8 {
-		return HystrixStream{}, errors.New("Event string too short to parse")
-	}
-
-	// The eventsource string isn't data
-	if s[:6] != "data: " {
-		return HystrixStream{}, errors.New("Can't parse non-data event")
-	}
-
-	// Try to parse JSON
-	var ret HystrixStream
-	resp := json.Unmarshal([]byte(s[7:]), &ret)
-
-	if resp == nil {
-		return ret, nil
-	} else {
-		return HystrixStream{}, resp
-	}
-}
-
 func (h HystrixHistogram) ToLatencyHistogram(mean int64) LatencyHistogram {
 	return LatencyHistogram {
 		Mean: mean,
